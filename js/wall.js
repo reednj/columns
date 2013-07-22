b09cbe47f275ef093dbb9e325eebb63b4cc9b34f
reednj <reednj@gmail.com>
2013-07-22 14:13:32 +0800

    algorithm created to calculate if the balls are separated or not

--- js/wall.js
+++ js/wall.js
@@ -10,7 +10,8 @@ var Game = new Class({
 
 
 		this.walls = [
-			new Wall({x: 300, y: 0})
+			new Wall({x: 300, y: 0}),
+			new Wall({x: 0, y: 250, length: 300}),
 		];
 
 		this.balls = [];
@@ -46,6 +47,12 @@ var Game = new Class({
 
 		this.balls.push(b);
 		this.mainCanvas.add(b);
+
+		if(this.areBallsSeparate()) {
+			$('sep').innerHTML = 'SEPARATE';
+		} else {
+			$('sep').innerHTML = 'JOINED';
+		}
 	},
 
 	update: function() {
@@ -71,6 +78,25 @@ var Game = new Class({
 				}
 			}
 		}
+	},
+
+	areBallsSeparate: function() {
+		var result = true;
+
+		if(this.balls.length <= 1) {
+			return true;
+		}
+
+		for(var i=0; i < this.balls.length; i++){
+			for(var j=0; j < this.balls.length; j++){
+				if(!this.balls[i].isSeparated(this.balls[j], this.walls)) {
+					result = false;
+					break;
+				}
+			}
+		}
+
+		return result;
 	}
 });
 
@@ -112,13 +138,35 @@ var Ball = new Class({
 			this.yv = -this.yv;
 		}
 
+	},
+
+	isSeparated: function(ball, walls) {
+		var result = false;
+
+		if(this == ball) {
+			return true;
+		}
+
+		// we need to check to make sure that there is at least one wall between
+		// this and the ball that was passed in. Between means that
+		// b1.x < w.x < b2.x for vertical walls, and within the y bounds of the wall
+		for(var i=0; i < walls.length; i++) {
+			var wall = walls[i];
+			if(wall.separates(this, ball)) {
+				result = true;
+				break;
+			}
+		}
+
+		return result;
+
 	}
 });
 
 var Wall = new Class({
 	initialize: function(options) {
 		this.options = options || {};
-		this.x = this.options.x || 100;
+		this.x = this.options.x || 0;
 		this.y = this.options.y || 0;
 		this.length = this.options.length || 600;
 	},
@@ -131,9 +179,9 @@ var Wall = new Class({
 		context.moveTo(this.x, this.y);
 
 		if(this.getDirection() == 'v') {
-			context.lineTo(this.x, this.length || canvas.height);
+			context.lineTo(this.x, (this.y + this.length) || canvas.height);
 		} else {
-			context.lineTo(this.length || canvas.width, this.y);
+			context.lineTo((this.x + this.length) || canvas.width, this.y);
 		}
 
 		context.stroke();
@@ -141,5 +189,36 @@ var Wall = new Class({
 
 	getDirection: function() {
 		return this.x == 0 ? 'h' : 'v';
+	},
+
+	separates: function(ball1, ball2) {
+		if(!this.inBounds(ball1) || !this.inBounds(ball2)) {
+			// both balls have to be in the bounds of the wall for it to separate them
+			// so if thats not the case, we can just return false right now
+			return false;
+		}
+
+		// if we get this far, then we know that both balls are in the bounds
+		// so all we need to check it that the the balls are on oppisite sides of the
+		// wall, in whatever direction it goes. If they are, then the wall separates
+		// the balls
+		if(this.getDirection() == 'v') {
+			var side1 = (this.x - ball1.x) / (this.x - ball1.x).abs();
+			var side2 = (this.x - ball2.x) / (this.x - ball2.x).abs();
+			return side1 != side2;
+		} else {
+			var side1 = (this.y - ball1.y) / (this.y - ball1.y).abs();
+			var side2 = (this.y - ball2.y) / (this.y - ball2.y).abs();;
+			return side1 != side2;
+		}
+
+
+	},
+
+	// for the ball to be in bounds of the wall means that it is within the lengthways coordinates of the wall
+	// that means, for a horizontal wall, a vertical line could be drawn from some point on the wall and it
+	// would intersect the ball.
+	inBounds: function(ball) {
+		return (this.getDirection() == 'v') ? ball.y >= this.y && ball.y <= this.y + this.length : ball.x >= this.x && ball.x <= this.x + this.length;
 	}
 });
