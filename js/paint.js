c1a8ca28e0b8e196d83b229a3886166357a0adb6
reednj <reednj@gmail.com>
2013-07-30 16:09:21 +0800

    allow the user to click and have the cells change color

--- js/paint.js
+++ js/paint.js
@@ -8,8 +8,9 @@ var Game = new Class({
 
 		this.grid = new CanvasGrid({
 			squareSize: this.squareSize,
-			rows: this.canvas.height / this.squareSize,
-			columns: this.canvas.width / this.squareSize
+			onCellSet: function() {
+				this.mainCanvas.refresh();
+			}.bind(this)
 		});
 
 		this.mainCanvas.add(this.grid);
@@ -42,12 +43,35 @@ var CanvasGrid = new Class({
 	initialize: function(options) {
 		this.options = options || {};
 		this.squareSize = this.options.squareSize || 30;
-		this.rows = this.options.rows || 0;
-		this.columns = this.options.columns || 0;
+		this.canvas = $(this.options.canvas) || $('main-canvas');
+		this.rows = this.options.rows || this.canvas.height / this.squareSize;
+		this.columns = this.options.columns || this.canvas.width / this.squareSize;
 
+		this.data = {};
 		this.offset = {x: 0, y: 0};
 		this.gridOrigin = {x:0,y:0};
 		this.topLeft = this.toGrid(0, 0);
+
+		this.options.onCellSet = this.options.onCellSet || function() {};
+
+		this.canvas.addEvent('click', function(e) {
+			var c = this.getCanvasCoords(e.client.x, e.client.y);
+			var g = this.toGrid(c.x, c.y);
+			this.setCell(g.gx, g.gy);
+		}.bind(this));
+	},
+
+	getCell: function(gx, gy) {
+		return this.data[this.getCellID(gx, gy)];
+	},
+
+	setCell: function(gx, gy, color) {
+		this.data[this.getCellID(gx, gy)] = color || '#222';
+		this.options.onCellSet(gx, gy, color);
+	},
+
+	getCellID: function(gx, gy) {
+		return gx.toString(16) + '-' + gy.toString(16);
 	},
 
 	// transfer the offset information into the origin, and start rendering
@@ -68,10 +92,30 @@ var CanvasGrid = new Class({
 	},
 
 	render: function(context, canvas) {
-		var extra = 10;
+
 		context.save();
-		context.strokeStyle = '#ccc';
 		context.translate(this.offset.x + this.gridOrigin.x % this.squareSize, this.offset.y + this.gridOrigin.y % this.squareSize);
+
+		for(var gx=this.topLeft.gx - 1; gx < this.topLeft.gx + this.columns + 1; gx++) {
+			for(var gy = this.topLeft.gy - 1; gy < this.topLeft.gy + this.rows + 1; gy++) {
+				if(!this.getCell(gx, gy)) {
+					continue;
+				}
+
+				var rectOrigin = this.fromGrid(gx, gy);
+				context.fillStyle = this.getCell(gx, gy);
+				context.fillRect(rectOrigin.x, rectOrigin.y, this.squareSize, this.squareSize);
+			}
+		}
+
+		this.renderGridLines(context, canvas);
+
+		context.restore();
+	},
+
+	renderGridLines: function(context, canvas) {
+		var extra = 10;
+		context.strokeStyle = '#ccc';
 
 		for(var i=-extra; i < (canvas.height / this.squareSize) + extra; i++) {
 			context.beginPath();
@@ -86,18 +130,6 @@ var CanvasGrid = new Class({
 			context.lineTo( i * this.squareSize, canvas.height +  this.squareSize*extra);
 			context.stroke();
 		}
-
-		context.font = '7pt Arial';
-		context.textAlign = 'center';
-
-		for(var gx=this.topLeft.gx-1; gx < this.topLeft.gx + this.columns + 1; gx++) {
-			for(var gy = this.topLeft.gy - 1; gy < this.topLeft.gy + this.rows + 1; gy++) {
-				var t = this.fromGrid(gx, gy);
-				context.fillText(gx + ',' + gy, t.x, t.y + 4);
-			}
-		}
-
-		context.restore();
 	},
 
 	toGrid: function(x, y) {
@@ -116,8 +148,23 @@ var CanvasGrid = new Class({
 	// 0,0 grid square.
 	fromGrid: function(gx, gy) {
 		return {
-			x: (this.gridOrigin.x - this.gridOrigin.x % this.squareSize) + gx * this.squareSize + this.squareSize / 2,
-			y: (this.gridOrigin.y - this.gridOrigin.y % this.squareSize) + gy * this.squareSize + this.squareSize / 2
+			x: (this.gridOrigin.x - this.gridOrigin.x % this.squareSize) + gx * this.squareSize,
+			y: (this.gridOrigin.y - this.gridOrigin.y % this.squareSize) + gy * this.squareSize
+		};
+	},
+
+	getCellCenter: function(gx, gy) {
+		var p = this.fromGrid(gx, gy);
+		return {
+			x: p.x + this.squareSize / 2,
+			y: p.y + this.squareSize / 2
+		};
+	},
+
+	getCanvasCoords: function(clientX, clientY) {
+		return {
+			x: clientX - this.canvas.offsetLeft,
+			y: clientY - this.canvas.offsetTop
 		};
 	}
 
