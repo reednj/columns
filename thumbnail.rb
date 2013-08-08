a6d7d49b5ab07fce356f940bfa712b43fc42a8c1
reednj <reednj@gmail.com>
2013-08-08 14:06:10 +0800

    test framework set up for the thumbnail generation

--- thumbnail.rb
+++ thumbnail.rb
@@ -7,22 +7,66 @@ class ColorHelper
 
 	def self.from_string(str)
 		ch = ColorHelper.new
-
-		if str.length == 4
-			ch.r = Integer(str[1] + str[1], 16) / 255.0
-			ch.g = Integer(str[2] + str[2], 16) / 255.0
-			ch.b = Integer(str[3] + str[3], 16) / 255.0
-		elsif str.length == 7
-			ch.r = Integer(str[1, 2], 16) / 255.0
-			ch.g = Integer(str[3, 2], 16) / 255.0
-			ch.b = Integer(str[5, 2], 16) / 255.0
-		end
-		
+		color = ColorHelper.normalize(str)
+		ch.r = Integer(color[1, 2], 16)
+		ch.g = Integer(color[3, 2], 16)
+		ch.b = Integer(color[5, 2], 16)
 		return ch
 	end
 
-	def intensity
-		return 0.299 * self.r + 0.587 * self.g + 0.114 * self.b
+	def self.normalize(str)
+		if str.length == 4
+			return str[1] + str[1] + str[2] + str[2] + str[3] + str[3]
+		else 
+			return str
+		end
+	end
+
+	def grayscale_teint
+		return (0.299 * self.r + 0.587 * self.g + 0.114 * self.b).round.to_i
+	end
+end
+
+class PaintMiniMap
+	@db = nil
+
+
+	def initialize(db)
+		@db = db
+		@block_width = 200
+		@block_height = 100
+	end
+
+	def get_filename(bx, by)
+		'thumb.' + bx.to_s + '.' + by.to_s + '.png'
+	end
+
+	def save_png(bx, by)
+		self.generate_png(bx, by).save(self.get_filename bx, by)
+	end
+
+	def generate_png(bx, by)
+		grid = self.block_to_grid(bx, by)
+		png = ChunkyPNG::Image.new(grid.width, grid.height, ChunkyPNG::Color::TRANSPARENT)
+		
+		@db[:cell].where('x >= ? && y >= ? && x < ? && y < ?', grid.x, grid.y, grid.x + grid.width, grid.y + grid.height).each do |cell|
+			image_x = cell[:x].to_i - grid.x
+			image_y = cell[:y].to_i - grid.y
+
+			gray = ColorHelper.from_string(cell[:color]).grayscale_teint
+			png[image_x, image_y] = ChunkyPNG::Color.grayscale(gray) 
+		end
+
+		return png
+	end
+
+	def block_to_grid(bx, by)
+		OpenStruct.new({
+			:x => (bx * @block_width) - @block_width / 2,
+			:y => (by * @block_height) - @block_height / 2,
+			:width => @block_width,
+			:height => @block_height
+		})
 	end
 end
 
@@ -30,26 +74,7 @@ class App
 	@@db = GamesDb.connect
 
 	def self.main
-
-		
-		board_size = 500
-		w = board_size
-		h = board_size
-		
-
-		data = get_blocks(board_size)
-		png = ChunkyPNG::Image.new(w, h, ChunkyPNG::Color::TRANSPARENT)
-
-		data.each_with_index  do |row, y|
-			row.each_with_index  do |intensity, x|
-				color = (intensity * 255).round
-				png[x, y] = ChunkyPNG::Color.rgb(color, color, color)
-			end			
-		end
-
-		
-
-		png.save('test.png')
+		PaintMiniMap.new(@@db).save_png(0, 0)
 	end
 
 	# gets a list of blocks for a square centered around the
