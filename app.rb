807918a8058e35ca9b7896334894429462c65804
Nathan <reednj@gmail.com>
2017-09-14 14:11:41 +0800

    make columns the only game

--- app.rb
+++ app.rb
@@ -1,18 +1,10 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'json'
 
-# pretty sure tihs needs to go last in order for the other
-# included files to reload properly
-require "sinatra/reloader" if development?
-
-# important - the cell data requests are large, but compress by about 90%
 use Rack::Deflater
 set :version, '1.0'
 
 get '/' do
-	redirect to('/columns')
-end
-
-get '/columns' do
 	erb :columns
 end
