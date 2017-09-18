27710daacb42fec2e42b78778abe86d5a1e112d4
Nathan <reednj@gmail.com>
2017-09-18 11:01:17 +0800

    update deployment script

--- config/pr-deploy.sh
+++ config/pr-deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-WEB=~/games.popacular.com
-SRC=~/code/columns.git
-CONFIG=~/code/config_backup/redditgold
-
-# copy the required files to the website
-rm -rf $WEB/*
-cp -R $SRC/* $WEB
-rm -rf $WEB/sh
-cp $CONFIG/db.rb $WEB/config/
+WEB=~/code/columns.git
 
 mkdir $WEB/tmp
 touch $WEB/tmp/restart.txt
