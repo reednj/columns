008deaa3d10c2f7996af3ffa2c88de28a03c93ca
Nathan <reednj@gmail.com>
2017-09-18 10:48:13 +0800

    allow the gamemodel to return the top scores

--- app.rb
+++ app.rb
@@ -9,8 +9,6 @@ Dir["./lib/**/*.rb"].each {|f| require f }
 use Rack::Deflater
 set :version, '1.0'
 
-REDIS = Redis.new
-
 get '/' do
 	erb :columns
 end
@@ -24,38 +22,3 @@ post '/game/incr/:id/:n' do |game_id, n|
 	"ok"
 end
 
-class GameModel
-	attr_accessor :game_id
-
-	def initialize(game_id)
-		self.game_id = game_id
-	end
-
-	def self.start
-		g = self.new String.random(28)
-		g.redis.zadd "columns:games:start", Time.now.to_i, g.game_id
-		return g
-	end
-
-	def self.[](game_id)
-		g = self.new game_id
-		g.exist!
-		return g
-	end
-
-	def incr(n)
-		self.redis.zincrby "columns:games:score", n.to_i, game_id
-	end
-
-	def exist?
-		!self.redis.zrank("columns:games:start", game_id).nil?
-	end
-
-	def exist!
-		raise 'game does not exist' unless exist?
-	end
-
-	def redis
-		REDIS
-	end
-end
---
+++ lib/models.rb
@@ -0,0 +1,48 @@
+require 'redis'
+REDIS = Redis.new
+
+class GameModel
+	attr_accessor :game_id
+	attr_accessor :score
+
+	def initialize(game_id)
+		self.game_id = game_id
+	end
+
+	def self.top(n=10)
+        data = REDIS.zrevrange "columns:games:score", 0, 9, :with_scores => true
+        data.map do |a| 
+            game = self.new a[0]
+            game.score = a[1]
+            game
+        end
+	end
+
+	def self.start
+		g = self.new String.random(28)
+		g.redis.zadd "columns:games:start", Time.now.to_i, g.game_id
+		return g
+	end
+
+	def self.[](game_id)
+		g = self.new game_id
+		g.exist!
+		return g
+	end
+
+	def incr(n)
+		self.redis.zincrby "columns:games:score", n.to_i, game_id
+	end
+
+	def exist?
+		!self.redis.zrank("columns:games:start", game_id).nil?
+	end
+
+	def exist!
+		raise 'game does not exist' unless exist?
+	end
+
+	def redis
+		REDIS
+	end
+end
